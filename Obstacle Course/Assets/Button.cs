d2dc198539a425728491d1bfdf528b395074f243
Unknown <janeyslinger@gmail.com>
2020-02-26 12:33:11 +0000

    Door puzzle implemented, moved player back to start, tested level

---
+++ Obstacle Course/Assets/Button.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public Door doorObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other){
+        doorObject.Open();
+    }
+
+    private void OnTriggerExit(Collider other){
+        doorObject.Close();
+    }
+}
